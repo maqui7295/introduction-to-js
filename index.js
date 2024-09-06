23ff2b756f9e9a5900b2ad5827904903842ce1e7
maqui7295 <medosa185@gmail.com>
2024-09-06 11:16:34 +0100

    strings, variables and scoping

--- index.js
+++ index.js
@@ -160,3 +160,248 @@ log(bigX + 2n)
 let y = 2n
 
 // https://devdocs.io/javascript/global_objects/bigint 
+
+// strings
+
+//  creation
+
+log("PERFORMING STRING CREATION")
+
+let firstName = "John"
+let lastName = 'Doe'
+let sentences = `This is the first line
+This is the second line
+This is the third line
+`
+
+let nickName = "Dwane 'The Rock' Johnson"
+let anotherNickName = 'Steve "Stone Cold" Austin'
+
+log(nickName)
+log(anotherNickName)
+
+nickName = "Dwane \"The Rock\" Johnson"
+anotherNickName = 'Steve \'Stone Cold\' Austin'
+
+log(nickName)
+log(anotherNickName)
+
+log("This is a line!!!!!\nThis is the second line!!!!")
+log("Name\tAge\tHeight\nJohn Doe\t14\t1.73m")
+log("C:\\Users\\medos\\OneDrive\\Desktop\\mark\\career\\classes\\Lucky\\files")
+
+log(firstName)
+log(lastName)
+log(sentences)
+
+log("PERFORMING STRING OPERATIONS")
+
+// operations
+let fullName = firstName + " " + lastName;
+fullName = `${firstName} ${lastName}` // string interpolation
+
+age = 45;
+
+let description = `His name is "${fullName}" and he is ${age} years old.
+2 + 2 = ${2 + 2}
+`;
+
+log(description)
+
+let mystring = "     Hello World!        "
+
+// mystring = mystring.trim()
+// let mystring2 = mystring.trim()
+
+log(mystring)
+log(mystring.trim())
+log(mystring.trimStart())
+log(mystring.trimEnd())
+
+const repeated = mystring.trim().repeat(5)
+log(repeated)
+
+log(repeated.replace("World", "Planet"))
+log(repeated.replace(/World/g, "Planet"))
+log(repeated.replaceAll("World", "Planet"))
+
+let greeting = "Hello World";
+
+log(greeting.length)
+
+log(greeting.charAt(10))
+log(greeting.charCodeAt(1))
+log(greeting.codePointAt(1))
+
+log(greeting.slice(0, 4))
+log(greeting.slice(3))
+log(greeting.substring(0, 4))
+
+log(sentences.split('\n'))
+log(sentences.split(' '))
+log(fullName.split(' '))
+
+// for loop...more in control flow
+for (let index = 0; index < greeting.length; index++) {
+    const char = greeting[index];
+    log(char)
+}
+
+
+// type conversions
+let age_str = age.toString()
+log(age_str, typeof age_str)
+log(typeof String(45))
+
+// You've seen these
+log(Number.parseInt("123"), Number.parseFloat("133.45"), Number.parseFloat("aaa"))
+
+let n = Number.parseFloat("aaa")
+
+log(Number.isNaN(n))
+
+// log(!!"")
+// log(!"")
+// log(!1)
+// log(!!age)
+// log(!![1, 3])
+// log(!![])
+// log(!![].length)
+
+// log(mystring.length === 0)
+// log(mystring === "")
+
+// truthy - true, 1, [], {}
+// falsy - false, 0, "", 
+
+// statement, expressions, variables
+
+// let statement -> let variableName = expression;
+let something = 2 + 2; 
+let z = 2 + 3;
+
+// function declaration/statement
+// function name(params) {
+    
+// }
+
+// function call expression
+log(age);
+
+// normal expressions
+2 + 2;
+
+// if Statement
+// if (condition) {
+    
+// }
+
+log(animal)
+
+
+// variables
+
+// naming - no keywords, must start with letter or _underscore, no spaces
+// pattern - snake_case, camelCase, PascalCase
+// let if = "" fails
+
+var animal = 'Dog';
+var animal = "Height"
+var animal = 4;
+
+let goodBoy = "Dog"
+// let goodBoy = "Dog"
+goodBoy = 34;
+goodBoy = "";
+
+// fails
+// anotherAnimal = 23;
+// anotherAnimal = "Tuwio";
+
+const pi = 3.142
+const anotherFish = "Tilapia"
+// const a;
+
+// multiple declarations/assignments
+let _123, a_12, my_name, myName, PascalCase;
+var p, q, r;
+
+var p;
+var q;
+var r;
+
+let wizard = "a", fish = "b";
+
+
+// scoping
+// if, else, for, while 
+{
+    var anotherAnimal = "Fish"
+}
+
+log(anotherAnimal)
+
+let arr = [1, 2, 3, 4, 5]
+
+for (var index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    log(element)
+}
+
+log(index)
+
+
+const human = "Tall"
+let humanAgain = "Short"
+
+log(human)
+log(humanAgain)
+
+{
+    const human = "Something else"
+    const humanAgain = "Again else"
+
+    log(human)
+    log(humanAgain)
+
+    let monkeyKing = "Wu Kong"
+}
+
+// log(monkeyKing)
+
+log(human)
+log(humanAgain)
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    log(element)
+}
+
+// log(i)
+
+function sayHello(name) {
+  var color = "red"
+
+  let human = 1
+  let humanAgain = 2;
+  
+  log(color, human, humanAgain);
+  console.log(`My name is ${name}`);
+}
+
+// continue from here
+function sayHelloAgain(name) {
+  var color = "red"
+
+  human = 1
+  humanAgain = 2;
+  
+  log(color, human, humanAgain);
+  console.log(`My name is ${name}`);
+}
+
+// log(color)
+sayHello()
+sayHello(fullName)
+
+sayHelloAgain(fullName)
