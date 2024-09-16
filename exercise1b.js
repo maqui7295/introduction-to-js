90d454da7b3562de9a34a2e47696322ba93a53ce
maqui7295 <medosa185@gmail.com>
2024-09-16 11:43:58 +0100

    exercise count unique words

---
+++ exercise1b.js
@@ -0,0 +1,8 @@
+// Exercise 2
+
+const text = `
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo error eaque iste sed maiores harum repudiandae nostrum reprehenderit repellat non, voluptate excepturi odio temporibus tempora nihil sapiente laudantium? Sit, ea!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo error eaque iste sed maiores harum repudiandae nostrum reprehenderit repellat non, voluptate excepturi odio temporibus tempora nihil sapiente laudantium? Sit, ea!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo error eaque iste sed maiores harum repudiandae nostrum reprehenderit repellat non, voluptate excepturi odio temporibus tempora nihil sapiente laudantium? Sit, ea!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo error eaque iste sed maiores harum repudiandae nostrum reprehenderit repellat non, voluptate excepturi odio temporibus tempora nihil sapiente laudantium? Sit, ea!
+`
+
+// Count all the unique words in text.
+// Example result. { the: 4, amet: 3 }
