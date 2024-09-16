d2241a8d839a80d35ffa3a6df5ade00c0c4eb188
maqui7295 <medosa185@gmail.com>
2024-09-16 11:40:09 +0100

    food store exercise 1 solution

--- exercise1.mjs
+++ exercise1.mjs
@@ -3,10 +3,8 @@
 // Setup
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-const rl = readline.createInterface({ input, output });
 
-
-const menu = {
+const ITEMS = {
     fish: '$200',
     eggs: '$100',
     rice: '$500',
@@ -17,6 +15,7 @@ const menu = {
     tomatoes: '$200',
     brocolli: '$300',
     onions: '$100',
+    beans: '$300',
 }
 
 // Your tasks
@@ -33,11 +32,76 @@ const menu = {
 
 // YOUR SOLUTION STARTS HERE
 
+
+// 1. Give the food store a name
+const storeName = "John Doe's Food Store"
+
+// 2. Print a welcome message that includes the store name
+console.log(`Welcome to ${storeName}\n`)
+
+// 3. Print the menu in a tabular format
+console.log("Here's a list of our store items");
+console.log('Name:\tPrice')
+Object.keys(ITEMS).forEach(function(name) {
+    console.log(`${name}:\t${ITEMS[name]}`);
+})
+
+console.log(); // put a new line
+
+// 4. Print a message asking the buyer to place an order. An order is a string separated of 
+// of items separated with spaces e.g "fish eggs"
+console.log("Place an order to purchase items from our store");
+
 // 5. Take orders
 // solution
+const rl = readline.createInterface({ input, output });
 const order = await rl.question('Enter your order separated with spaces: ');
 rl.close();
 
-console.log(order);
+// console.log(order);
 
-// Your solution continues
+let orders = order.trim().split(' ');
+
+orders = orders.filter(function(ord) {
+    return ord !== "";
+})
+
+// console.log(orders);
+
+
+// 6. Calculate the total price.
+let totalFee = 0;
+
+const itemNames = Object.keys(ITEMS)
+const validOrders = [];
+
+orders.forEach(function(foodItem) {
+  // 7. If the buyer includes an item that's absent from the menu, print a message and ignore that order e.g. in "fish toothpaste", ignore toothpaste 
+  foodItem = foodItem.toLowerCase();
+
+  if (itemNames.includes(foodItem)) {
+    
+    // select price
+    let price = ITEMS[foodItem];
+    
+    // convert price from a string to an integer
+    price = price.replace("$", "");
+    price = Number.parseInt(price);
+    
+    totalFee += price;
+
+    // store the valid orders
+    validOrders.push(foodItem);
+    
+  } else {
+    console.log(`${foodItem} is not part of our items. Skipping...................`);
+  }
+
+})
+
+// 8. Print the order and total price.
+console.log(`Your fee for ${validOrders.join(' and ')} is $${totalFee}`);
+
+// 9. Print a good bye message
+console.log(`Thank you for purchasing at ${storeName}`);
+
